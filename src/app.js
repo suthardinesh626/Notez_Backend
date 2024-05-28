d6d6a5b2c24135b1a0b44fb38fe6c8ee227688f8
Dinesh Suthar <suthardinesh626@gmail.com>
2024-05-28 22:40:13 +0530

    upadting before deployment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+// import path from 'path';
 
 const app = express();
 
@@ -10,20 +10,20 @@ app.use(cors({
     credentials: true,
 }));
 
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser());
 
+// const buildpath = path.join(__dirname, "./client/build");
+// app.use(express.static(buildpath));
+
+app.use(cookieParser());
 
 //routes import 
 import userRouter from './routes/user.routes.js';
 import noteRouter from './routes/note.routes.js';
 
-
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/note", noteRouter)
-
 
 export { app };
