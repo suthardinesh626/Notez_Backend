a624544f801eeba325782885fd0b37e41191287e
Dinesh Suthar <suthardinesh626@gmail.com>
2024-06-01 18:32:24 +0530

    api changed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-// import path from 'path';
 
 const app = express();
 
@@ -14,9 +13,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
-// const buildpath = path.join(__dirname, "./client/build");
-// app.use(express.static(buildpath));
 
 
 //routes import 
